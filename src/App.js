67eba8f554bca017ae1f8a10e39f5f6973c51a8d
bondanjody <bondanjodys@gmail.com>
2023-03-21 17:05:44 +0700

    Menyempurnakan penggunaan custom hooks pada komponen App. Yaitu dengan menggunakan useCallback, dan membungkus kode di dalam useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -6,22 +6,20 @@ import useHttp from './hooks/use-http';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
-  const transformTasks = taskObj => {
-    const loadedTasks = [];
-
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-
-      setTasks(loadedTasks);
-  }
-
-  const {isLoading, error, sendRequest: fetchTasks} = useHttp({url: 'https://react-http-request-ab44d-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  const {isLoading, error, sendRequest: fetchTasks} = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = taskObj => {
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+  
+        setTasks(loadedTasks);
+    };
+    fetchTasks({url: 'https://react-http-request-ab44d-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
